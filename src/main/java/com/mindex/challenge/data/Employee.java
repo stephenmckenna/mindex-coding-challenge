7188b8adac53d919b2f6dcb133a1143981e30a64
mindex <dev@mindex.com>
2025-02-28 15:51:31 -0500

    reconfigure to use lombok annotations

--- src/main/java/com/mindex/challenge/data/Employee.java
+++ src/main/java/com/mindex/challenge/data/Employee.java
@@ -1,7 +1,18 @@
 package com.mindex.challenge.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
 import java.util.List;
 
+/*
+    Using Lombok annotations to include setters, getters, and necessary constructors
+    allows for cleaner, more concise code
+ */
+
+@Data
+@AllArgsConstructor
 public class Employee {
     private String employeeId;
     private String firstName;
@@ -11,53 +22,7 @@ public class Employee {
     private List<Employee> directReports;
 
     public Employee() {
+        this.directReports = new ArrayList<>();
     }
 
-    public String getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(String employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public List<Employee> getDirectReports() {
-        return directReports;
-    }
-
-    public void setDirectReports(List<Employee> directReports) {
-        this.directReports = directReports;
-    }
 }
